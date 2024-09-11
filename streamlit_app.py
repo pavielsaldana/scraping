b1624b6bac0a2bce7609b5bbfbbfb77dc66ac60f
pavielsaldana <124369853+pavielsaldana@users.noreply.github.com>
2024-09-11 18:29:53 +0000

    ai_title_cleaning

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,20 +7,29 @@ if not os.path.exists("/home/appuser/.cache/ms-playwright"):
 st.set_page_config(page_title="ABM App", page_icon="https://media.licdn.com/dms/image/v2/C4E0BAQEUNQJN0rf-yQ/company-logo_200_200/company-logo_200_200/0/1630648936722/kalungi_inc_logo?e=2147483647&v=beta&t=4vrP50CSK9jEFI7xtF7DzTlSMZdjmq__F0eG8IJwfN8")
 
 welcome_page = st.Page("streamlit_scripts/streamlit_welcome.py",
-                       title="Welcome", icon = ":material/account_circle:")
+                       title="Welcome",
+                       icon = ":material/account_circle:")
 linkedin_scraping_page = st.Page("streamlit_scripts/streamlit_linkedin_scraping.py",
-                                title="LinkedIn scraping",  icon = ":material/table_chart:")
+                                title="LinkedIn scraping",
+                                icon = ":material/table_chart:")
 linkedin_search_page = st.Page("streamlit_scripts/streamlit_linkedin_search.py",
-                               title = "LinkedIn search",  icon = ":material/search:")
+                               title = "LinkedIn search", 
+                               icon = ":material/search:")
 linkedin_outreach_page = st.Page("streamlit_scripts/streamlit_linkedin_outreach.py",
-                                 title = "LinkedIn outreach", icon = ":material/supervisor_account:")
+                                 title = "LinkedIn outreach",
+                                 icon = ":material/supervisor_account:")
 ai_qa_page = st.Page("streamlit_scripts/streamlit_ai_qa.py",
-                                 title = "AI QA", icon = ":material/checklist_rtl:")
+                                 title = "AI QA",
+                                 icon = ":material/checklist_rtl:")
+ai_title_cleaning_page = st.Page("streamlit_scripts/streamlit_ai_title_cleaning.py",
+                                 title = "AI title cleaning",
+                                 icon = ":material/face:")
 pg = st.navigation(
     {
         "Welcome": [welcome_page,],
         "LinkedIn scripts": [linkedin_scraping_page, linkedin_search_page, linkedin_outreach_page],
         "Enrichment scripts": [ai_qa_page],
+        "Data cleaning": [ai_title_cleaning_page],
     }
 )
 
