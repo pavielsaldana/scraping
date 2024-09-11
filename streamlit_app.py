8fdb8ac4298acbe6fcaab8297aee96bcb2367f18
pavielsaldana <124369853+pavielsaldana@users.noreply.github.com>
2024-09-11 18:21:28 +0000

    ai

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,15 @@ if not os.path.exists("/home/appuser/.cache/ms-playwright"):
 st.set_page_config(page_title="ABM App", page_icon="https://media.licdn.com/dms/image/v2/C4E0BAQEUNQJN0rf-yQ/company-logo_200_200/company-logo_200_200/0/1630648936722/kalungi_inc_logo?e=2147483647&v=beta&t=4vrP50CSK9jEFI7xtF7DzTlSMZdjmq__F0eG8IJwfN8")
 
 welcome_page = st.Page("streamlit_scripts/streamlit_welcome.py",
-                       title="Welcome",)
+                       title="Welcome", icon = ":material/account_circle:")
 linkedin_scraping_page = st.Page("streamlit_scripts/streamlit_linkedin_scraping.py",
-                                title="LinkedIn scraping",)
+                                title="LinkedIn scraping",  icon = ":material/table_chart:")
 linkedin_search_page = st.Page("streamlit_scripts/streamlit_linkedin_search.py",
-                               title = "LinkedIn search",)
+                               title = "LinkedIn search",  icon = ":material/search:")
 linkedin_outreach_page = st.Page("streamlit_scripts/streamlit_linkedin_outreach.py",
-                                 title = "LinkedIn outreach",)
+                                 title = "LinkedIn outreach", icon = ":material/supervisor_account:")
 ai_qa_page = st.Page("streamlit_scripts/streamlit_ai_qa.py",
-                                 title = "AI QA",)
+                                 title = "AI QA", icon = ":material/checklist_rtl:")
 pg = st.navigation(
     {
         "Welcome": [welcome_page,],
