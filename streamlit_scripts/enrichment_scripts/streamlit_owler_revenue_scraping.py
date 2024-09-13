339d4b075908103ea4650a74cc696d4d56fb1ba7
pavielsaldana <124369853+pavielsaldana@users.noreply.github.com>
2024-09-13 00:30:31 +0000

    owler

--- streamlit_scripts/enrichment_scripts/streamlit_owler_revenue_scraping.py
+++ streamlit_scripts/enrichment_scripts/streamlit_owler_revenue_scraping.py
@@ -60,7 +60,7 @@ if owler_revenue_option != "Select one Owler revenue script":
                         write_into_spreadsheet(spreadsheet_url, sheet_name_result, dataframe_results, key_dict)
                     if owler_revenue_option == "Scraping Owler URLs":
                         dataframe_results = scraping_owler_urls(dataframe_input, domain_column_name, zenrowsApiKey, owler_column_name)
-                        write_into_spreadsheet(spreadsheet_url, sheet_name_result, dataframe_search_results, key_dict)
+                        write_into_spreadsheet(spreadsheet_url, sheet_name_result, dataframe_results, key_dict)
                     st.success("Scraping completed!")
             except Exception as e:
                 st.error(f"An error occurred: {e}")
