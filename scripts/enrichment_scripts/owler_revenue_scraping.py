0437340711d4fe74120b8a7c0b5df8c43f5fa97e
pavielsaldana <124369853+pavielsaldana@users.noreply.github.com>
2024-09-13 00:18:35 +0000

    ss

--- scripts/enrichment_scripts/owler_revenue_scraping.py
+++ scripts/enrichment_scripts/owler_revenue_scraping.py
@@ -34,6 +34,8 @@ def get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--lang=en-US")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     return webdriver.Chrome(
