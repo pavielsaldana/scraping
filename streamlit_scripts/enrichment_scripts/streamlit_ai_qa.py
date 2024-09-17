fdcd863c2bcefe651fc6c246a1c38a447fd9a90d
juliomayaudon <120761472+juliomayaudon@users.noreply.github.com>
2024-09-17 12:22:24 -0500

    Update streamlit_ai_qa.py

--- streamlit_scripts/enrichment_scripts/streamlit_ai_qa.py
+++ streamlit_scripts/enrichment_scripts/streamlit_ai_qa.py
@@ -214,6 +214,23 @@ def process_data(spreadsheet_url, sheet_name, column_name, formatted_keywords, p
 #STREAMLIT BEGIN
 st.title("QA with Searching Keyword")
 
+option = st.selectbox(
+    "Select a Client ICP",
+    ("Select Scraper Type","New ICP Fit QA","Headlight Solutions (Chemical)")
+)
+
+if option == "New ICP Fit QA":
+    keywords_input= "Keyword1_to_search, Keyword2_to_search, Keyword3_to_search..."
+    prompt_input= "Assess if the company is a XXXXXX by searching for terms or phrases indicating this kind of services  including but not limited to XXXXXX. Respond in the following manner: Yes. Provide a brief explanation (no more than 300 characters) on why it qualifies. No. Provide a brief explanation (no more than 300 characters) on why it does not qualify. Maybe. If the information is ambiguous or insufficient, briefly explain (no more than 300 characters) why it's not possible to determine."
+    verticals_input= "Vertical1: \"Keyword1\", \"Keyword2\", \"Keyword3\"...,\n"
+                               "Vertical2: \"Keyword1\", \"Keyword2\", \"Keyword3\"..."
+
+elif option == "Headlight Solutions (Chemical)":
+    keywords_input= "Delivery, Shipping, last mile, White Glove, final mile"
+    prompt_input= "Assess if the company is a manufacturer or provides any delivery or shipping of Chemical products or derivatives by searching for terms or phrases indicating this kind of services  including but not limited to 'Chemical Distributors', 'Chemical Manuufacturers', 'Shipping', 'Delivery'. Respond in the following manner: Yes. Provide a brief explanation (no more than 300 characters) on why it qualifies. No. Provide a brief explanation (no more than 300 characters) on why it does not qualify. Maybe. If the information is ambiguous or insufficient, briefly explain (no more than 300 characters) why it's not possible to determine."
+    verticals_input= "Shipping: \"Shipping\", \"Delivery\", \"Logistics\", \"Freight\"\n"
+                               "Chemicals: \"Chemical\", \"Chemicals\", \"Hazardous Materials\""
+
 st.write("Use the IA QA tool when you have a list of domains that you need to do QA to check if the companies are fit with the ICP, you can also check if there are mention of certain keywords in the webpages.")
 st.write("[Tutorial >](https://www.loom.com/looms/videos)")
 
@@ -222,32 +239,31 @@ sheet_name = st.text_input("Select the Sheet Name", "Test")
 column_name = st.text_input("Select the Column Name", "domain")
 serper_api = st.text_input("Select a Serper API", "091de71c94b24d78f85f38e527c370ae6c2f2f59")
 
-keywords = st.text_area("Enter keywords separated by commas", "Delivery, Shipping, last mile, White Glove, final mile")
+keywords = st.text_area("Enter keywords separated by commas", keywords_input)
 keywords_list = [keyword.strip() for keyword in keywords.split(',')]
 keywords_final = ['"' + keyword + '"' for keyword in keywords_list]
 formatted_keywords = " | ".join(keywords_final)
 st.write("Formatted Keywords:", formatted_keywords)
 
-prompt = st.text_area("Enter the prompt", "Assess if the company is a manufacturer or provides any delivery or shipping of Chemical products or derivatives by searching for terms or phrases indicating this kind of services  including but not limited to 'Chemical Distributors', 'Chemical Manuufacturers', 'Shipping', 'Delivery'. Respond in the following manner: Yes. Provide a brief explanation (no more than 300 characters) on why it qualifies. No. Provide a brief explanation (no more than 300 characters) on why it does not qualify. Maybe. If the information is ambiguous or insufficient, briefly explain (no more than 300 characters) why it's not possible to determine.")
+prompt = st.text_area("Enter the prompt", prompt_input)
 
-verticals = st.text_area("Enter the verticals and their keywords", 
-                               "Shipping: \"Shipping\", \"Delivery\", \"Logistics\", \"Freight\"\n"
-                               "Chemicals: \"Chemical\", \"Chemicals\", \"Hazardous Materials\"")
+verticals = st.text_area("Enter the verticals and their keywords", verticals_input)
 
 if verticals:
     vertical_dict = process_vertical_input(verticals)
-    
-if st.button("Start processing"):
-    if not spreadsheet_url or not serper_api:
-        st.error("Please enter both the Spreadsheet URL and the Serper API key")
-    else:
-        with st.spinner("Running the scraper. This could take a few minutes depending on the list size..."):
-            try:
-                progress_bar = st.progress(0)
-                result, totalcost = process_data(spreadsheet_url, sheet_name, column_name, formatted_keywords, prompt, serper_api, progress_bar, key_dict, OPENAI_API_KEY, vertical_dict)
-                st.success("Scraping completed!")
-                st.dataframe(result)
-                st.write(f"El costo total fue: ${totalcost:.6f}")
-            except Exception as e:
-                st.error(f"An error occurred: {str(e)}")
-                st.exception(e)
+
+if option != "Select Client ICP":   
+    if st.button("Start processing"):
+        if not spreadsheet_url or not serper_api:
+            st.error("Please enter both the Spreadsheet URL and the Serper API key")
+        else:
+            with st.spinner("Running the scraper. This could take a few minutes depending on the list size..."):
+                try:
+                    progress_bar = st.progress(0)
+                    result, totalcost = process_data(spreadsheet_url, sheet_name, column_name, formatted_keywords, prompt, serper_api, progress_bar, key_dict, OPENAI_API_KEY, vertical_dict)
+                    st.success("Scraping completed!")
+                    st.dataframe(result)
+                    st.write(f"El costo total fue: ${totalcost:.6f}")
+                except Exception as e:
+                    st.error(f"An error occurred: {str(e)}")
+                    st.exception(e)
