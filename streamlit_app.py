cb63f7d727d14ee1bf34630ddb7ec8d385e98447
pavielsaldana <124369853+pavielsaldana@users.noreply.github.com>
2024-09-24 10:15:35 -0500

    rollback

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,16 +50,12 @@ if check_password():
     apollo_enrichment_page = st.Page("streamlit_scripts/enrichment_scripts/streamlit_apollo_enrichment.py",
                         title="Apollo enrichment",
                         icon=":material/checklist_rtl:")
-    anvil_works_test_page = st.Page("streamlit_scripts/enrichment_scripts/streamlit_anvil_works_test.py",
-                        title="anvil.works test",
-                        icon=":material/checklist_rtl:")
     pg = st.navigation(
         {
             "Welcome": [welcome_page,],
             "LinkedIn scripts": [linkedin_scraping_page, linkedin_search_page, linkedin_outreach_page,],
             "Enrichment scripts": [ai_qa_page, owler_revenue_page,company_linkedin_url_search_using_serper_page,apollo_enrichment_page,],
             "Data cleaning": [ai_title_cleaning_page,],
-            "Test": [anvil_works_test_page]
         }
     )
     st.logo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYXyGNY0mQSyCRUKXrXWI4-O31kspcM0eVLg&s")
